615f298ed0c703b54cbd2be8e2d8e8d9cf67df4e
dasWarder <Al.94_94@mail.ru>
2021-09-01 11:57:32 +0300

    Add OAuth2.0 with github

--- src/main/java/com/example/security_demo/repository/UserDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.security_demo.repository;
-
-import com.example.security_demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsRepository extends JpaRepository<User, String> {
-
-}
